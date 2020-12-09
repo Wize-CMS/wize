60f24bbbc789a2f5ea571f19f45acb3328d9ee74
João Ribeiro <jlribeiroaugusto@gmail.com>
2020-12-09 18:20:05 -0300

    [ADJUST]: Project Re-structed, now using composer and new struct directories

--- App/Interfaces/Controller/IController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Controller;
-
-interface iController {
-    public function render(string $path, ?array $data = []): string;
-};
--- App/config/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('PATH', $_SERVER['PWD']);
-
-define('INSTALL_PATH', PATH . '/App/install.php');
-
